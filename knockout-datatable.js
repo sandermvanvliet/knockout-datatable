f629b90bdff569bd2728c74e6168ab286843c1ef
Sander van Vliet <sauron@barad-dur.nl>
2015-03-18 15:10:10 +0100

    Add the ko parameter for the KnockoutJS dependency

--- knockout-datatable.js
+++ knockout-datatable.js
@@ -1,4 +1,4 @@
-define(["knockoutjs"], function() {
+define(["knockoutjs"], function(ko) {
   var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   this.DataTable = (function() {
